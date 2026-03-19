78179fa8c37826e9eec68e1ad23f40956c67d87b
Andrii <andrii.lavreniuk@oa.edu.ua>
2026-03-19 18:24:05 +0200

    Comment out base URL configuration for deployment flexibility

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/practice1/",
+  // Закоментуйте або видаліть рядок нижче, якщо деплоїте на сайтах, які працюють від кореня домену (наприклад, Vercel)
+  // base: "/practice1/",
   plugins: [react()],
 });
