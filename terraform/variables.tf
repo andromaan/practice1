b6aa7cbc20872196f2702ba4ce355fc14e67d20b
Andrii <andrii.lavreniuk@oa.edu.ua>
2026-04-03 11:49:12 +0300

    Merge branch 'branch-for-github-actions' into lab6

---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "vercel_api_token" {
+  description = "Vercel API token for authentication"
+  type        = string
+}
+variable "student_id" {
+  description = "Student ID for custom domain"
+  type        = string
+}
